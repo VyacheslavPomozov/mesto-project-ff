e21fd96b7688804b7a8da3495b90bf1f96e39e78
Вячеслав Помозов <polymer13@yandex.ru>
2024-08-08 20:13:55 +0200

    Улучшены функции, добавлено описание в тег alt

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,31 @@
 "use strict";
 
-let content = document.querySelector(".content");
-let placesList = content.querySelector(".places__list");
+const content = document.querySelector(".content");
+const placesList = content.querySelector(".places__list");
 const cardTemplate = document.querySelector("#card-template").content;
 
-function deleteCard(event) {
+function createCard(data, onDelete) {
+  const cardElement = cardTemplate
+    .querySelector(".places__item")
+    .cloneNode(true);
+  const deleteButton = cardElement.querySelector(".card__delete-button");
+
+  cardElement.querySelector(".card__image").setAttribute("src", data.link);
+  cardElement.querySelector(".card__image").setAttribute("alt", `Фотография: ${data.name}`);
+  cardElement.querySelector(".card__title").textContent = data.name;
+
+  deleteButton.addEventListener("click", onDelete);
+  return cardElement;
+}
+
+function handleDeleteCard(event) {
   event.target.closest(".places__item").remove();
 }
 
-function addCard(cards, deleteFunction) {
-  cards.forEach(function (item) {
-    const cardElement = cardTemplate
-      .querySelector(".places__item")
-      .cloneNode(true);
-    const deleteButton = cardElement.querySelector(".card__delete-button");
-
-    cardElement.querySelector(".card__image").setAttribute("src", item.link);
-    cardElement.querySelector(".card__title").textContent = item.name;
-    placesList.append(cardElement);
-
-    deleteButton.addEventListener("click", deleteFunction);
-  });
+function renderCard(create) {
+  placesList.append(create);
 }
 
-addCard(initialCards, deleteCard);
+initialCards.forEach((card) => {
+  renderCard(createCard(card, handleDeleteCard));
+});
